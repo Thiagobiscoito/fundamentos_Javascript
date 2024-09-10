1b056edfc5289d99d97d400e7934f2b0cd6066fd
Thiago Biscoito <thiagobiscoito182@hotmail.com>
2024-09-10 16:19:12 -0300

    variaveis constantes e tipos de dados

---
+++ programa.js
@@ -0,0 +1,30 @@
+//https://www.youtube.com/watch?v=aA31cVca_hI
+
+// VARIÁVEIS, CONSTANTES E TIPOS DE DADOS
+// NUMBER(NÚMERO)
+const x = 10;
+const y = 2.5;
+// STRING (SEQUENCIA DE CARACTERES)
+const nome = "Thiago";
+const sobrenome = "Reduzino";
+// BOLLEAN (BOOLEANO)
+const verdadeiro = true;
+const falso = false;
+// NULO
+const nulo = null;
+// NAO DEFINIDO
+const nao_definido = undefined;
+
+// TYPEOF INFORMA QUAL E O TIPO DE DADO DA VARIAVEL,
+// ex: 
+console.log(typeof nome);
+
+
+console.log(x);
+console.log(y);
+console.log(nome);
+console.log(sobrenome);
+console.log(verdadeiro);
+console.log(falso);
+console.log(nulo);
+console.log(nao_definido);
